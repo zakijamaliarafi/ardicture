59e81cf0d34b9d7cb71e2cb00fa48767b219c232
Zaki Jamali Arafi <zakijamaliarafi@gmail.com>
2024-05-04 15:18:28 +0700

    new schema for post_tag table

---
+++ database/migrations/2024_05_04_072804_create_post_tag_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->foreignId('post_id')->constrained(table: 'posts', column: 'post_id')->cascadeOnDelete();
+            $table->foreignId('tag_id')->constrained(table: 'tags', column: 'tag_id')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_tag');
+    }
+};
