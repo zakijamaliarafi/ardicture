7c19c8c5b6cc1363f8f6e935f7229fef88d6fba7
Casul63 <ikhsanhafidz09@gmail.com>
2024-05-31 10:47:31 +0700

    Adjusting Reports Page

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -4,46 +4,49 @@
         @php
             $i = 1;
         @endphp
-        <table class="border table-fixed border-collapse border-black ms-6">
-            <thead>
-                <tr>
-                    <th class="border border-slate-600 py-1 px-2">No.</th>
-                    <th class="border border-slate-600 py-1 px-2">Report Description</th>
-                    <th class="border border-slate-600 py-1 px-2">Reported By</th>
-                    <th class="border border-slate-600 py-1 px-2">Reported Post</th>
-
-                </tr>
-                @foreach ($reports as $report)
-                    <tr>
-                        <td class="border border-slate-600 py-1 px-2">{{ $i }}</td>
-                        <td class="border border-slate-600 py-1 px-2">{{ $report->report_description }}</td>
-                        <td class="border border-slate-600 py-1 px-2">{{ $report->user->name }}</td>
-                        <td class="border border-slate-600 py-1 px-2 flex items-center">
-                            <button class="bg-blue-500 text-white px-2 py-1 mr-2 rounded-md">
-                                <a href="{{ route('posts.show', $report->post->id) }}">View Post</a>
-                            </button>
-
-                            <form action="{{ route('reports.destroy', $report->id) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="bg-yellow-500 text-white px-2 py-1 mr-2 rounded-md">Delete
-                                    Report</button>
-                            </form>
-
-                            <form action="{{ route('posts.destroy', $report->post->id) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="bg-red-500 text-white px-2 py-1 rounded-md">Delete
-                                    Post</button>
-                            </form>
-                        </td>
-                    </tr>
-                    @php
-                        $i++;
-                    @endphp
-                @endforeach
-
+        <table class="table-fixed border-collapse ms-6 w-[400px] md:w-[750px] lg:w-[1000px]">
+            <thead class="bg-gray-300">
+                <tr class="">
+                    <th class="py-4 px-2 w-1/12">No</th>
+                    <th class="py-1 px-2 w-1/6">Description</th>
+                    <th class="py-1 px-2 w-1/6">Reported By</th>
+                    <th class="py-1 w-7/12">Action</th>
             </thead>
+            </tr>
+            @foreach ($reports as $report)
+                <tr>
+                    <td class="py-4 px-2 text-center text-orange-600">{{ $i }}</td>
+                    <td class="py-4 px-2 text-center text-orange-600">{{ $report->report_description }}</td>
+                    <td class="py-4 px-2 text-center text-orange-600">{{ $report->user->name }}</td>
+                    <td class="py-4 px-10 flex items-center justify-between">
+                        <button
+                            class="text-orange-600 px-5 py-1 mr-2 rounded-md border hover:bg-orange-600 hover:text-white border-orange-600">
+                            <a href="{{ route('posts.show', $report->post->id) }}">View Post</a>
+                        </button>
+
+                        <form action="{{ route('reports.destroy', $report->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit"
+                                class="bg-black text-white px-6 py-1 mr-2 hover:bg-slate-700 rounded-md">Delete
+                                Report</button>
+                        </form>
+
+                        <form action="{{ route('posts.destroy', $report->post->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit"
+                                class="bg-red-500 text-white hover:bg-red-700 px-4 py-1 rounded-md">Delete
+                                Post</button>
+                        </form>
+                    </td>
+                </tr>
+                @php
+                    $i++;
+                @endphp
+            @endforeach
+
+
         </table>
     </div>
 </x-layout>
